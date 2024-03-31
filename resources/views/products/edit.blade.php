57763156c2b24a8cb28a59ea802fa46636fd0410
JackF007 <you@example.com>
2024-04-01 00:18:23 +0200

    css + json fixes

--- resources/views/products/edit.blade.php
+++ resources/views/products/edit.blade.php
@@ -29,9 +29,9 @@ integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJ
         @csrf        
         @method('put')
         
-        <div class="form-group">
+        <div class="form-group mb-5">
             <label for="category_id">Categoria</label>
-            <select class="form-control" id="category_id" name="category_id">
+            <select class="form-control mb-2" id="category_id" name="category_id">
                 <option value="">Seleziona una Categoria</option>
                 @foreach($categories as $category)
                     <option value="{{ $category->id }}" {{ $product->category_id == $category->id ? 'selected' : '' }}>
